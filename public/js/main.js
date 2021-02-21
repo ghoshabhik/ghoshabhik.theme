4768ef3dfa21071f951bf58365d636ec06c601ba
abhik <avik5324@gmail.com>
2021-02-21 19:44:19 +0530

    build the front page

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,11 @@
-$(document).ready(function(){
-    $(".target-class").html("Showing All Projects");
-    //console.log($("#flag").val())
-    if($("#flag").val()=='on'){
-        $("#htmlOnly").attr("checked","")
-    }
-    
-})
-
-
-
-var htmlFlagToggle = document.querySelector("#htmlOnly")
-if(htmlFlagToggle){
-    htmlFlagToggle.addEventListener('click', (e) =>{
-        var flagElement = document.querySelector("#flag")
-        //console.log(flagElement)
-        if(flagElement.value == 'off') flagElement.value = 'on'
-        else flagElement.value = 'off'
-        //console.log('input text: ',flagElement.value)
-    })
-}
-
-
 document.addEventListener('DOMContentLoaded', (event) => {
     var target = window.location.pathname.split('/')[1]
     //console.log(target)
 
-    if(target){
+    if (target) {
         var elements = document.querySelectorAll(".nav-item");
         //console.log(elements)
-        elements.forEach.call(elements, function(el) {
+        elements.forEach.call(elements, function (el) {
             el.classList.remove("active");
         });
 
@@ -36,26 +13,26 @@ document.addEventListener('DOMContentLoaded', (event) => {
         //console.log(targetElement)
         targetElement.classList.add("active")
     }
-    
-  })
+
+})
 
 
-  $(".nav .nav-link").on("click", function(){
+$(".nav .nav-link").on("click", function () {
 
-    
+
     $(".nav").find(".active").removeClass("active");
     $(this).addClass("active");
     $(".target-class").html($(this).text());
     var selected = $(this).text()
     //console.log("Selected>>>>",selected )
-    if(selected != "All Projects"){
+    if (selected != "All Projects") {
         var elements = $(".card").find(".card-body").find("div.row").find("div.proj-type").find("div.text-muted")
         //console.log(elements)
-        elements.each(function(){
+        elements.each(function () {
             $(this).parent().parent().parent().parent().show()
             //console.log("InnerText======",$(this).text())
             //console.log("Selected::::",selected )
-            if($(this).text() != selected){
+            if ($(this).text() != selected) {
                 //console.log($(this).parent().parent())
                 $(this).parent().parent().parent().parent().hide()
             }
@@ -63,11 +40,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
     else {
         var elements = $(".card").find(".card-body").find("div.row").find("div.proj-type").find("div.text-muted")
-        elements.each(function(){
-                $(this).parent().parent().parent().parent().show()
-            }
+        elements.each(function () {
+            $(this).parent().parent().parent().parent().show()
+        }
         )
     }
-    
- });
- 
+
+});
+
+
